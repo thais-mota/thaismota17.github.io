df590dba6220dbc8485a20ed5d473f925656fe8a
Thais Mota <thais.mota@outlook.com>
2019-11-06 15:12:15 -0300

    WIP: Develop grades calculation for university

---
+++ scripts/calcular.js
@@ -0,0 +1,21 @@
+// function changeBackground(color) {
+//     document.body.style.background = color;
+//  }
+ 
+//  window.addEventListener("load",function() { changeBackground('black') });
+
+// $(function() {
+//     var colors = ["#0099cc","#c0c0c0","#587b2e","#990000","#000000","#1C8200","#987baa","#981890","#AA8971","#1987FC","#99081E"];
+
+//     setInterval(function() { 
+//         var bodybgarrayno = Math.floor(Math.random() * colors.length);
+//         var selectedcolor = colors[bodybgarrayno];
+//         $("body").css("background",selectedcolor);
+//     }, 3000);
+// })
+
+
+function submit_grade() {
+    var n1 = document.getElementById("n1").value;
+    document.getElementById('answer').innerHTML = "Você precisa de " + n1 + " para obter a média necessária para passar.";
+}
